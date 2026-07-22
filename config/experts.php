8d350a0c1e414e0d9acdb50214b775e433a69131
Danis <82171151+Skyredingsan@users.noreply.github.com>
2026-07-22 08:39:42 +0300

    Update experts.php

--- config/experts.php
+++ config/experts.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  * Координаторы видят все категории.
  */
 return [
-    'ПП' => ['SimonovaYS@fuel.ru', 'LarionovaTA@fuel.ru'],
+    'ПП' => ['SimonovaYuS@fuel.ru', 'LarionovaTA@fuel.ru'],
     'ОЭК' => ['VelichkinaSV@fuel.ru'],
     'ЭКЛ' => ['TihonovaML@fuel.ru', 'VelichkinaSV@fuel.ru'],
     'КБ' => ['SmirnovaLS@fuel.ru'],
